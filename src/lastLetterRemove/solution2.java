ffbd0f7c3f097d8e9f4e2288e07d8cef70075ddb
Kirsti Schaffer <kirstischaffer@gmail.com>
2021-08-12 19:22:57 -0500

    lastLetterRemove solution2complete

---
+++ src/lastLetterRemove/solution2.java
@@ -0,0 +1,12 @@
+package lastLetterRemove;
+//Create a function that takes a string input parameter and prints out that string with its last letter missing.
+//        Examples of input and output:
+//        — Passing in “laboratory” should print out “laborator”
+//        — Passing in “proof” should print out “proo”
+//        — Passing in “interference” should print out “interferenc”
+public class solution2 {
+    public static void main(String[] args){
+        String word = "interference";
+        System.out.println(word.substring(0,word.length()-1));
+    }
+}
