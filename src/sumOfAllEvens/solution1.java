cb56b973ba690da1073d88221fc7a6ca4ba2e43d
kschaffer7 <kirstischaffer@gmail.com>
2021-08-14 17:34:34 -0500

    sumOfAllEvens updated

--- src/sumOfAllEvens/solution1.java
+++ src/sumOfAllEvens/solution1.java
@@ -4,12 +4,12 @@ package sumOfAllEvens;
 // from 0 up to and including the integer passed in the parameter.
 //Examples of input and output:
 //— Passing in 5 should print out “6”.
-//— Passing in 10 should print out “27”.
-//— Passing in 15 should print out “50”.
+//— Passing in 10 should print out “30”.
+//— Passing in 15 should print out “56”.
 
 public class solution1 {
     public static void main(String[] args){
-        int number = 10;
+        int number = 15;
         int evensSum = 0;
         for(int i = 0; i <= number; i++){
             if(i%2 == 0){
